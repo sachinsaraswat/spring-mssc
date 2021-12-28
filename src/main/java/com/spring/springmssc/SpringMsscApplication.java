821b0f8f3202efc356cffc9b4272c1741516b913
Sachin Saraswat <sswt.git@gmail.com>
2021-12-28 18:44:08 -0500

    Init

---
+++ src/main/java/com/spring/springmssc/SpringMsscApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springmssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMsscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springmssc/SpringMsscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springmssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
